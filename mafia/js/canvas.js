0e467c34d043520a16c482850bfc5d49cf104268
RITUL <rituljain.nitt@gmail.com>
2016-02-24 00:18:28 +0530

    beta_2

--- mafia/js/canvas.js
+++ mafia/js/canvas.js
@@ -3,24 +3,30 @@ $(document).ready(function() {
 	var context = canvas.get(0).getContext("2d");
 	var canvasWidth = canvas.width();
 	var canvasHeight = canvas.height();
+	
 	$(window).resize(resizeCanvas);//awesome;
 	function resizeCanvas() {	
-		canvas.attr("width", $(window).get(0).innerWidth);
+		canvas.attr("width", $(window).get(0).innerWidth-50);
 		canvas.attr("height", $(window).get(0).innerHeight);
 		canvasWidth = canvas.width();
 		canvasHeight = canvas.height();
 	};
 	resizeCanvas();
+	// alert(canvasWidth);
+	// alert(canvasHeight);
 	context.fillStyle = "rgb(255, 255, 255)";
 	context.fillRect(0,0,canvasWidth,canvasHeight);
 	var img=document.getElementById('hello');
 	var playAnimation=true;
 	var i=0;
 	var count=Math.random()*1000;
+	var p=canvas.offset().top-$(window).scrollTop();
+	//alert(p);
+	
 	function bullet(){
 		i++;
 		var temp=Math.random();
-		var x=-100+temp*1600;
+		var x=-100+temp*1200;
 		var temp2=Math.random();
 		var y=-100+temp2*1000;
 		console.log(x,y);
@@ -35,7 +41,7 @@ $(document).ready(function() {
 		}
 	};
 	if(playAnimation){
-	var k= setInterval(bullet,100);
+	var k= setInterval(bullet,130);
 	}
 	
 	
